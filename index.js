e9a7b43900636d4fa2e675e1de477d913a59c4e1
EmanuelCAC <emanuelc279@gmail.com>
2024-01-05 16:35:44 -0300

    backend finished

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./server.js";
 import mongodb from "mongodb";
-//import ReviewsDAO from "./dao/reviewsDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
 
 const MongoClient = mongodb.MongoClient;
 const mongo_username = process.env['MONGO_USERNAME'];
@@ -10,18 +10,19 @@ const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.uxtmnyy.
 const port = 8000;
 
 MongoClient.connect(
-    uri,
-    {
-        maxPoolSize: 50,
-        wtimeoutMS: 2500
+  uri,
+  {
+    maxPoolSize: 50,
+    wtimeoutMS: 2500
+  })
+  .catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async client => {
+    await ReviewsDAO.injectDB(client);
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
     })
-    .catch(err => {
-        console.error(err.stack);
-        process.exit(1);
-    })
-    .then(async client => {
-        app.listen(port, () => {
-            console.log(`listening on port ${port}`);
-        })
-    })
+  })
 
